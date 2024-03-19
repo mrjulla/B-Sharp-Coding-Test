1cba67924790ee8b155463e916df4fccae30b690
ibumjlp <ibumjlp@hotmail.com>
2024-03-19 12:51:23 +0700

    updated

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # bsharp-quiz
 
 This project is currently deprecated.
+
+## Installation
+
+To install the project, follow these steps:
+
+1. Clone the repository:
+```
+git clone https://github.com/ibumjlp/bsharp-quiz.git
+```
+
+2. Navigate to the project directory:
+```
+cd bsharp-quiz
+```
+
+3. Install dependencies:
+```
+npm i
+```
+
+## Usage
+
+To run the project, use the following command:
+
+```
+node app.js
+```
+
+## Running the Application with Docker
+
+To run the application using Docker, follow these steps:
+
+1. Build the Docker image:
+```
+docker build -t my-app .
+```
+
+2. Run the Docker container:
+```
+docker run -d -p 8080:8080 my-app
+```
+
+This will start the Docker container in detached mode, and your application will be accessible at http://localhost:8080.
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var restify = require('restify');
 var errs = require('restify-errors');
 
-const server = restify.createServer();
+const server = restify.createServer({
+    name: 'myapp',
+    version: '1.0.0'
+});
 
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 function cal(operator, x, y) {
@@ -18,7 +23,7 @@ function cal(operator, x, y) {
             answer = parseFloat(x) * parseFloat(y);
             break;
         case "/":
-            if(y == "0") {
+            if (y == "0") {
                 answer = "Divide by zero";
                 break;
             } else {
@@ -34,19 +39,19 @@ function cal(operator, x, y) {
 server.post('/calculator', function (req, res, next) {
     let cal_res = cal(req.body.operator, req.body.x, req.body.y);
 
-    if(cal_res == "Divide by zero") {
+    if (cal_res == "Divide by zero") {
         return next(new errs.BadRequestError("Divide by zero"));
     } else if (cal_res == "Invalid Operator") {
         return next(new errs.BadRequestError("Invalid operator"));
     } else {
-        const obj = { value:cal_res }
+        const obj = { value: cal_res }
         res.send(obj);
         return next();
     }
 });
 
 server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
 
 module.exports = cal;
